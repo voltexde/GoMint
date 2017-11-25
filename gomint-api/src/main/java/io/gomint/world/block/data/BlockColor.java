513ffa53d606ce24b853766ab0ad4a02c45d036b
Fabian Faßbender <fabian.fassbender42@googlemail.com>
2017-11-25 22:47:09 +0100

    Add block color to hardened clay

---
+++ gomint-api/src/main/java/io/gomint/world/block/data/BlockColor.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2017, GoMint, BlackyPaw and geNAZt
+ *
+ * This code is licensed under the BSD license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package io.gomint.world.block.data;
+
+/**
+ * @author geNAZt
+ * @version 1.0
+ */
+public enum BlockColor {
+
+    WHITE,
+    ORANGE,
+    MAGENTA,
+    LIGHT_BLUE,
+    YELLOW,
+    LIME,
+    PINK,
+    GRAY,
+    LIGHT_GRAY,
+    CYAN,
+    PURPLE,
+    BLUE,
+    BROWN,
+    GREEN,
+    RED,
+    BLACK
+
+}
