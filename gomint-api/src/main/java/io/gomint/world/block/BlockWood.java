14e6e8ae040a6ce1dd4d642c6ffba3cb83af6529
Fabian Faßbender <fabian.fassbender42@googlemail.com>
2018-03-28 10:33:42 +0200

    Add API for WoodType. Adds #161

--- gomint-api/src/main/java/io/gomint/world/block/BlockWood.java
+++ gomint-api/src/main/java/io/gomint/world/block/BlockWood.java
@@ -12,4 +12,28 @@ package io.gomint.world.block;
  * @version 1.0
  */
 public interface BlockWood extends Block {
+
+    /**
+     * Get wood type
+     *
+     * @return wood type
+     */
+    WoodType getWoodType();
+
+    /**
+     * Set the wood type
+     *
+     * @param woodType which should be used in this block
+     */
+    void setWoodType( WoodType woodType );
+
+    enum WoodType {
+        OAK,
+        SPRUCE,
+        BIRCH,
+        JUNGLE,
+        ACACIA,
+        DARK_OAK
+    }
+
 }
--- gomint-server/src/main/java/io/gomint/server/world/block/Wood.java
+++ gomint-server/src/main/java/io/gomint/server/world/block/Wood.java
@@ -1,9 +1,13 @@
 package io.gomint.server.world.block;
 
-import io.gomint.world.block.BlockType;
-
-import io.gomint.inventory.item.*;
+import io.gomint.inventory.item.ItemDiamondAxe;
+import io.gomint.inventory.item.ItemGoldenAxe;
+import io.gomint.inventory.item.ItemIronAxe;
+import io.gomint.inventory.item.ItemStack;
+import io.gomint.inventory.item.ItemStoneAxe;
+import io.gomint.inventory.item.ItemWoodenAxe;
 import io.gomint.server.registry.RegisterInfo;
+import io.gomint.world.block.BlockType;
 
 /**
  * @author geNAZt
@@ -25,11 +29,11 @@ public class Wood extends Block implements io.gomint.world.block.BlockWood {
     @Override
     public Class<? extends ItemStack>[] getToolInterfaces() {
         return new Class[]{
-                ItemWoodenAxe.class,
-                ItemIronAxe.class,
-                ItemDiamondAxe.class,
-                ItemGoldenAxe.class,
-                ItemStoneAxe.class
+            ItemWoodenAxe.class,
+            ItemIronAxe.class,
+            ItemDiamondAxe.class,
+            ItemGoldenAxe.class,
+            ItemStoneAxe.class
         };
     }
 
@@ -46,6 +50,52 @@ public class Wood extends Block implements io.gomint.world.block.BlockWood {
     @Override
     public boolean canBeBrokenWithHand() {
         return true;
+    }
+
+    @Override
+    public WoodType getWoodType() {
+        switch ( this.getBlockData() ) {
+            case 5:
+                return WoodType.DARK_OAK;
+            case 4:
+                return WoodType.ACACIA;
+            case 3:
+                return WoodType.JUNGLE;
+            case 2:
+                return WoodType.BIRCH;
+            case 1:
+                return WoodType.SPRUCE;
+            case 0:
+            default:
+                return WoodType.OAK;
+        }
+    }
+
+    @Override
+    public void setWoodType( WoodType woodType ) {
+        switch ( woodType ) {
+            case DARK_OAK:
+                this.setBlockData( (byte) 5 );
+                break;
+            case ACACIA:
+                this.setBlockData( (byte) 4 );
+                break;
+            case JUNGLE:
+                this.setBlockData( (byte) 3 );
+                break;
+            case BIRCH:
+                this.setBlockData( (byte) 2 );
+                break;
+            case SPRUCE:
+                this.setBlockData( (byte) 1 );
+                break;
+            case OAK:
+            default:
+                this.setBlockData( (byte) 0 );
+                break;
+        }
+
+        this.updateBlock();
     }
 
 }
