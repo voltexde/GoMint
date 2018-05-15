c08c289c0c1d1e8f674818405b0dfda89c761d25
Fabian Faßbender <fabian.fassbender42@googlemail.com>
2018-05-15 17:41:39 +0200

    Fix a crash when asking for out of bounds block generators

--- gomint-server/src/main/java/io/gomint/server/registry/Registry.java
+++ gomint-server/src/main/java/io/gomint/server/registry/Registry.java
@@ -121,8 +121,12 @@ public class Registry<R> {
     }
 
     public final R getGenerator( int id ) {
-        if ( id < 0 ) return this.negativeGenerators[id * -1];
-        return this.generators[id];
+        if ( id < 0 ) {
+            id *= -1;
+            return this.negativeGenerators.length < id ? null : this.negativeGenerators[id];
+        }
+
+        return this.generators.length < id ? null : this.generators[id];
     }
 
     public int getId( Class<?> clazz ) {
