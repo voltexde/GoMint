3dd0168e5e45f0c3948b1ebbb162c1f684601bc3
Kaooot <37877491+Kaooot@users.noreply.github.com>
2018-08-10 21:15:24 +0200

    Add 3 new torches (#317)

---
+++ gomint-server/src/main/java/io/gomint/server/inventory/item/ItemUnderwaterTorch.java
@@ -0,0 +1,17 @@
+package io.gomint.server.inventory.item;
+
+import io.gomint.inventory.item.ItemType;
+import io.gomint.server.registry.RegisterInfo;
+
+/**
+ * @author Kaooot
+ * @version 1.0
+ */
+@RegisterInfo( id = 239 )
+public class ItemUnderwaterTorch extends ItemStack implements io.gomint.inventory.item.ItemUnderwaterTorch {
+
+    @Override
+    public ItemType getType() {
+        return ItemType.UNDERWATER_TORCH;
+    }
+}
---
+++ gomint-server/src/main/java/io/gomint/server/world/block/UnderwaterTorch.java
@@ -0,0 +1,120 @@
+package io.gomint.server.world.block;
+
+import io.gomint.math.AxisAlignedBB;
+import io.gomint.server.registry.RegisterInfo;
+import io.gomint.server.world.block.state.BlockfaceBlockState;
+import io.gomint.world.block.BlockType;
+import io.gomint.world.block.BlockUnderwaterTorch;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Kaooot
+ * @version 1.0
+ */
+@RegisterInfo( id = 239 )
+public class UnderwaterTorch extends Block implements BlockUnderwaterTorch {
+
+    private BlockfaceBlockState facing = new BlockfaceBlockState();
+
+    @Override
+    public void generateBlockStates() {
+        // Facing
+        this.facing.fromData( (byte) ( this.getBlockData() & 0x05 ) );
+    }
+
+    @Override
+    public int getBlockId() {
+        return 239;
+    }
+
+    @Override
+    public boolean isTransparent() {
+        return true;
+    }
+
+    @Override
+    public boolean isSolid() {
+        return false;
+    }
+
+    @Override
+    public float getBlastResistance() {
+        return 0.0f;
+    }
+
+    @Override
+    public BlockType getType() {
+        return BlockType.UNDERWATER_TORCH;
+    }
+
+    @Override
+    public boolean canBeBrokenWithHand() {
+        return true;
+    }
+
+    @Override
+    public long getBreakTime() {
+        return 0;
+    }
+
+    @Override
+    public boolean canPassThrough() {
+        return true;
+    }
+
+    @Override
+    public List<AxisAlignedBB> getBoundingBox() {
+        float size = 0.15f;
+
+        switch ( this.facing.getState() ) {
+            case EAST:
+                return Collections.singletonList( new AxisAlignedBB(
+                    this.location.getX(),
+                    this.location.getY() + 0.2f,
+                    this.location.getZ() + 0.5f - size,
+                    this.location.getX() + size * 2f,
+                    this.location.getY() + 0.8f,
+                    this.location.getZ() + 0.5f + size
+                ) );
+            case WEST:
+                return Collections.singletonList( new AxisAlignedBB(
+                    this.location.getX() + 1.0f - size * 2f,
+                    this.location.getY() + 0.2f,
+                    this.location.getZ() + 0.5f - size,
+                    this.location.getX() + 1f,
+                    this.location.getY() + 0.8f,
+                    this.location.getZ() + 0.5f + size
+                ) );
+            case SOUTH:
+                return Collections.singletonList( new AxisAlignedBB(
+                    this.location.getX() + 0.5f - size,
+                    this.location.getY() + 0.2f,
+                    this.location.getZ(),
+                    this.location.getX() + 0.5f + size,
+                    this.location.getY() + 0.8f,
+                    this.location.getZ() + size * 2f
+                ) );
+            case NORTH:
+                return Collections.singletonList( new AxisAlignedBB(
+                    this.location.getX() + 0.5f - size,
+                    this.location.getY() + 0.2f,
+                    this.location.getZ() + 1f - size * 2f,
+                    this.location.getX() + 0.5f + size,
+                    this.location.getY() + 0.8f,
+                    this.location.getZ() + 1f
+                ) );
+        }
+
+        size = 0.1f;
+        return Collections.singletonList( new AxisAlignedBB(
+            this.location.getX() + 0.5f - size,
+            this.location.getY() + 0.0f,
+            this.location.getZ() + 0.5f - size,
+            this.location.getX() + 0.5f + size,
+            this.location.getY() + 0.6f,
+            this.location.getZ() + 0.5f + size
+        ) );
+    }
+}
