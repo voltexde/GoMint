89cbf56caf158e864028f93b069f2fd0e52a5413
Clockw1seLrd <clockw1selrd@gmail.com>
2018-10-04 00:18:33 +0200

    Fully implement chunk generator registry

--- gomint-server/src/main/java/io/gomint/server/world/generator/SimpleChunkGeneratorRegistry.java
+++ gomint-server/src/main/java/io/gomint/server/world/generator/SimpleChunkGeneratorRegistry.java
@@ -1,6 +1,7 @@
 package io.gomint.server.world.generator;
 
 import com.google.common.base.Preconditions;
+import io.gomint.server.util.performance.ObjectConstructionFactory;
 import io.gomint.world.World;
 import io.gomint.world.generator.ChunkGenerator;
 import io.gomint.world.generator.ChunkGeneratorRegistry;
@@ -8,6 +9,7 @@ import io.gomint.world.generator.GeneratorContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Field;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,19 +18,65 @@ public class SimpleChunkGeneratorRegistry implements ChunkGeneratorRegistry {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SimpleChunkGeneratorRegistry.class);
 
-    private final Map<String, Class<? extends ChunkGeneratorRegistry>> registeredGenerators = new HashMap<>();
     private final Map<String, Class<? extends ChunkGenerator>> registeredGenerators = new HashMap<>();
+
+    public SimpleChunkGeneratorRegistry() {}
+
+    public SimpleChunkGeneratorRegistry(Class<? extends ChunkGenerator>... initialGeneratorClasses) {
+        for (Class<? extends ChunkGenerator> initialGeneratorClass : initialGeneratorClasses) {
+            this.registerGenerator(initialGeneratorClass);
+        }
+    }
 
     @Override
     public ChunkGenerator createGenerator(String name, World world, GeneratorContext ctx) {
-        return null;
+        Preconditions.checkNotNull(name, "'name' cannot be null");
+        Preconditions.checkNotNull(world, "'world' cannot be null");
+        Preconditions.checkNotNull(ctx, "'ctx' cannot be null");
+
+        if (!this.isGeneratorAvailable(name)) {
+            throw new IllegalStateException("No such chunk generator for '" + name + "'");
+        }
+
+        Class<? extends ChunkGenerator> generatorClass = this.registeredGenerators.get(name);
+        ObjectConstructionFactory factory = new ObjectConstructionFactory(generatorClass, World.class,
+                                                                          GeneratorContext.class);
+
+        ChunkGenerator generator = (ChunkGenerator) factory.newInstance(world, ctx);
+
+        if (generator == null) {
+            throw new IllegalStateException("Failed constructing chunk generator - Passed parameters:\n"
+                                                + ".. " + world.getClass().getName() + "\n"
+                                                + ".. " + ctx.getClass().getName() + "\n");
+        }
+
+        return generator;
     }
 
     @Override
     public boolean registerGenerator(Class<? extends ChunkGenerator> generatorClass) {
         Preconditions.checkNotNull(generatorClass, "'generatorClass' cannot be null");
 
-        return false;
+        String generatorName;
+
+        try {
+            ObjectConstructionFactory factory = new ObjectConstructionFactory(generatorClass, String.class, World.class,
+                                                                              GeneratorContext.class);
+            Object generatorInstance = factory.newInstance(null, null, null);
+
+            Field nameField = generatorClass.getDeclaredField("name");
+            generatorName = (String) nameField.get(generatorInstance);
+        } catch (NoSuchFieldException | IllegalAccessException cause) {
+            LOGGER.error("Failed to register '" + generatorClass.getName() + "':", cause);
+            return false;
+        }
+
+        if (this.isGeneratorAvailable(generatorName)) {
+            throw new IllegalStateException("Chunk generator '" + generatorName + "' is already registered");
+        }
+
+        this.registeredGenerators.put(generatorName, generatorClass);
+        return true;
     }
 
     @Override
