337ccc9a5f374081dde1229a139fc2b9dfb88816
Kevims <kcodeyt@gmail.com>
2018-10-17 14:48:22 +0200

    Fix Typo in PlayerActionHandler (#371)
    * Fix module names for IntelliJ
    
    * Update GoMint for 1.7
    
    * Add other things for 1.7
    
    * Add Spinning
    
    * Fix Typo xd

--- gomint-server/src/main/java/io/gomint/server/network/handler/PacketPlayerActionHandler.java
+++ gomint-server/src/main/java/io/gomint/server/network/handler/PacketPlayerActionHandler.java
@@ -62,6 +62,32 @@ public class PacketPlayerActionHandler implements PacketHandler<PacketPlayerActi
 
             case STOP_SPIN_ATTACK:
                 if ( connection.getEntity().isSwimming() ) {
+                    PlayerSpinEvent playerSpinEvent = new PlayerSpinEvent( connection.getEntity(), true );
+                    connection.getServer().getPluginManager().callEvent( playerSpinEvent );
+                    if ( playerSpinEvent.isCancelled() ) {
+                        connection.getEntity().sendData( connection.getEntity() );
+                    } else {
+                        connection.getEntity().setSpinning( false );
+                    }
+                }
+
+                break;
+
+            case START_SPIN_ATTACK:
+                if ( !connection.getEntity().isSpinning() ) {
+                    PlayerSpinEvent playerSpinEvent = new PlayerSpinEvent( connection.getEntity(), true );
+                    connection.getServer().getPluginManager().callEvent( playerSpinEvent );
+                    if ( playerSpinEvent.isCancelled() ) {
+                        connection.getEntity().sendData( connection.getEntity() );
+                    } else {
+                        connection.getEntity().setSpinning( true );
+                    }
+                }
+
+                break;
+
+            case STOP_SPIN_ATTACK:
+                if ( connection.getEntity().isSpinning() ) {
                     PlayerSwimEvent playerSwimEvent = new PlayerSwimEvent( connection.getEntity(), false );
                     connection.getServer().getPluginManager().callEvent( playerSwimEvent );
                     if ( playerSwimEvent.isCancelled() ) {
