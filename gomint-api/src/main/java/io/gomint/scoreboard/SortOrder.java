2c4039d53dd6ec7ba1669f9e16e255072f43f19d
Kevims <kcodeyt@gmail.com>
2018-10-26 02:52:19 +0200

    Adding the ability of setting the sort order of a scoreboard

---
+++ gomint-api/src/main/java/io/gomint/scoreboard/SortOrder.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2018, GoMint, BlackyPaw and geNAZt
+ *
+ * This code is licensed under the BSD license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package io.gomint.scoreboard;
+
+/**
+ * @author KCodeYT
+ * @version 1.0
+ */
+public enum SortOrder {
+
+    ASCENDING,
+    DESCENDING
+
+}
--- gomint-test-plugin/src/main/java/io/gomint/plugin/listener/PlayerJoinListener.java
+++ gomint-test-plugin/src/main/java/io/gomint/plugin/listener/PlayerJoinListener.java
@@ -1,13 +1,15 @@
 package io.gomint.plugin.listener;
 
+import io.gomint.GoMint;
 import io.gomint.event.EventHandler;
 import io.gomint.event.EventListener;
 import io.gomint.event.EventPriority;
 import io.gomint.event.player.PlayerJoinEvent;
-import io.gomint.inventory.item.ItemDiamondSword;
-import io.gomint.inventory.item.ItemDye;
-import io.gomint.inventory.item.data.DyeType;
 import io.gomint.plugin.TestPlugin;
+import io.gomint.scoreboard.DisplaySlot;
+import io.gomint.scoreboard.Scoreboard;
+import io.gomint.scoreboard.ScoreboardDisplay;
+import io.gomint.scoreboard.SortOrder;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -23,12 +25,14 @@ public class PlayerJoinListener implements EventListener {
     public void onPlayerJoin( PlayerJoinEvent event ) {
         // Set to allow all permissions
         event.getPlayer().getPermissionManager().setPermission( "*", true );
-        event.getPlayer().setLevel( 45 );
-        event.getPlayer().getInventory().setItem( 0, ItemDiamondSword.create( 1 ) );
 
-        ItemDye dye = ItemDye.create( 64 );
-        dye.setDyeType( DyeType.LAPIS_LAZULI );
-        event.getPlayer().getInventory().setItem( 1, dye );
+        Scoreboard scoreboard = GoMint.instance().createScoreboard();
+        ScoreboardDisplay display = scoreboard.addDisplay( DisplaySlot.SIDEBAR, "test", "TEST", SortOrder.DESCENDING );
+
+        display.addLine( "test1", 0 );
+        display.addLine( "test2", 1 );
+
+        event.getPlayer().setScoreboard( scoreboard );
     }
 
 }
