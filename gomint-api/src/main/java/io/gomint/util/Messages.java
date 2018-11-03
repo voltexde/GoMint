0883511d466000fd385fbb761bf740b5333d8421
Clockw1seLrd <clockw1selrd@gmail.com>
2018-11-03 02:01:19 +0100

    Add utility for easily generating non null messages

---
+++ gomint-api/src/main/java/io/gomint/util/Messages.java
@@ -0,0 +1,19 @@
+package io.gomint.util;
+
+import com.google.common.base.Preconditions;
+
+/**
+ * @author Clockw1seLrd
+ * @version 1.0
+ */
+public class Messages {
+
+    private static final String PARAM_IS_NULL_FORMAT = "Passed parameter '%s' cannot be null";
+
+    public static String paramIsNull( String paramName ) {
+        Preconditions.checkNotNull( paramName, String.format( PARAM_IS_NULL_FORMAT, "paramName" ) );
+
+        return String.format( PARAM_IS_NULL_FORMAT, paramName );
+    }
+
+}
