3916bbc72b975bf3ec42e3b0c366f87e17eea132
Kaooot <37877491+Kaooot@users.noreply.github.com>
2018-11-03 11:34:06 +0100

    Add effects to an enchanted golden apple if the player consuming this item (#420)
    * Add effects to an enchanted golden apple if the player consuming this item
    
    * changed timeunits

--- gomint-server/src/main/java/io/gomint/server/inventory/item/ItemEnchantedGoldenApple.java
+++ gomint-server/src/main/java/io/gomint/server/inventory/item/ItemEnchantedGoldenApple.java
@@ -1,8 +1,12 @@
 package io.gomint.server.inventory.item;
+import io.gomint.entity.potion.PotionEffect;
 import io.gomint.inventory.item.ItemType;
 
+import io.gomint.server.entity.EntityPlayer;
 import io.gomint.server.registry.RegisterInfo;
 import io.gomint.taglib.NBTTagCompound;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author geNAZt
@@ -21,6 +25,17 @@ public class ItemEnchantedGoldenApple extends ItemFood implements io.gomint.inve
     @Override
     public float getHunger() {
         return 4;
+    }
+
+    @Override
+    public void onConsume( EntityPlayer player ) {
+        super.onConsume( player );
+
+        // Apply effects
+        player.addEffect( PotionEffect.REGENERATION, 4, 30, TimeUnit.SECONDS );
+        player.addEffect( PotionEffect.DAMAGE_RESISTANCE, 0, 5, TimeUnit.MINUTES );
+        player.addEffect( PotionEffect.FIRE_RESISTANCE, 0, 5, TimeUnit.MINUTES );
+        player.addEffect( PotionEffect.ABSORPTION, 3, 2, TimeUnit.MINUTES );
     }
 
     @Override
